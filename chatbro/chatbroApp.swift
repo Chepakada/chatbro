928e786c0501c3fb8255bfad9a86b19d61363aca
Prashant Lamichhane <prashantlamichhane68@gmail.com>
2023-06-13 09:51:14 -0600

    Initial Commit

---
+++ chatbro/chatbroApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbroApp.swift
+//  chatbro
+//
+//  Created by Prashant Lamichhane on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct chatbroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
